30456eba2dabc850e034aa6349d672e6d4723010
Suurow <benjamin.breyne@hotmail.com>
2022-11-11 11:25:34 +0100

    website.s

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var typed = new Typed('#typed', {
+    strings: [
+        "Je suis étudiant à l'ESIEE-IT",
+        "En alternance dans le Groupe Monnoyeur"
+      ],
+    typeSpeed: 60,
+    backDelay: 1000,
+    loop: true
+  });
