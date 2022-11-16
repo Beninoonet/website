b83d0b4657ff9c41d76c698d655e1fbdf13bbdfa
Suurow <benjamin.breyne@hotmail.com>
2022-11-16 14:27:47 +0100

    za

--- js/main.js
+++ js/main.js
@@ -3,9 +3,11 @@ const closemenu = document.getElementById('closemenu')
 const menu = document.getElementById('menu')
 
 openmenu.addEventListener('click', () => {
-    menu.style.display = "flex";
+    menu.style.display = "flex"
 })
 
 closemenu.addEventListener('click', () => {
-    menu.style.display = "none";
+    menu.style.display = "none"
 })
+
+
