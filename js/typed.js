ca5f40711d70aad00530ff9d078d8c120b0e270d
Beninoonet <beninoonet@pm.me>
2022-11-20 14:44:50 +0100

    text typed modifications

--- js/typed.js
+++ js/typed.js
@@ -2,8 +2,6 @@ var typed = new Typed('#typed', {
     strings: [
         "E-girl préférée de ta e-girl préférée.",
         "Créateur de contenus.",
-        "j'ai 20 ans.",
-        "j'ai pas le bac.",
         "Hôte d'un refuge sur discord."
       ],
     typeSpeed: 80,
